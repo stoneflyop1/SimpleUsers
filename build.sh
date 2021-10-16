e9a6cbb2adb282ab0f090728da0f173e8f534bc4
Jifeng <jeff-zhang@outlook.com>
2021-10-16 16:40:01 +0800

    update bash

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
-version = (git rev-parse --short=8 HEAD)
-sqlpass = "123456"
+#!/bin/bash
+version=(git rev-parse --short=8 HEAD)
+sqlpass="123456"
 ## mysql db
 cd ./sql
 docker build --no-cache --build-arg SQL_PASS=$sqlpass -t dotnetusers/db:$version .
@@ -8,6 +8,6 @@ docker run -d -p 3306:3306 --name dotnetuserdb dotnetusers/db:$version --charact
 
 ## webapp
 cd ..
-connStr = "Server=dotnetuserdb;Database=dotnetusers;Uid=root;Pwd=123456;CHARSET=utf8;";
+connStr="Server=dotnetuserdb;Database=dotnetusers;Uid=root;Pwd=123456;CHARSET=utf8;";
 docker build --no-cache --build-arg CONN_STR=$connStr -t dotnetusers/webapp .
 docker run -d -p 5000:80 --link dotnetuserdb --name dotnetuserapp dotnetusers/webapp
